f258ee72fd605b9103fff6c918ad34907087d67a
cold <wh_linux@126.com>
2017-04-18 22:13:33 +0800

    Adjust setup.py to disable pbr generates AUTHORS & ChangeLog

--- setup.py
+++ setup.py
@@ -3,7 +3,12 @@
 """Setup"""
 from __future__ import print_function, division, unicode_literals
 
+import os
+
 from setuptools import setup
+
+os.environ["SKIP_GENERATE_AUTHORS"] = "1"
+os.environ["SKIP_WRITE_GIT_CHANGELOG"] = "1"
 
 setup(
     setup_requires=["pbr>=1.9", "setuptools>=17.1"],
